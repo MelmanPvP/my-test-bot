03ad1396489e1e828b39aa6d72f2efb73454ad5b
Dima <dtsarev05@gmail.com>
2025-09-08 22:18:49 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -45,15 +45,19 @@ bot.command('remind', async (ctx) => {
     let delay = 0;
     switch (unit) {
         case 'с':
+        case "s":
             delay = value * 1000;
             break;
         case 'м':
+        case "m":
             delay = value * 1000 * 60;
             break;
         case 'ч':
+        case "h":
             delay = value * 1000 * 60 * 60;
             break;
-        case 'д': delay = value * 1000 * 60 * 60 * 24;
+        case 'д':
+        case "d": delay = value * 1000 * 60 * 60 * 24;
     }
     ctx.reply(`Ок, напомню через ${value} ${unit}.`);
     setTimeout(() => {
